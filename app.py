a1204a139b5a8262ba08f51ca71dc73764f0ca0d
davutkara1985-create <davutkara1985@gmail.com>
2026-05-14 16:10:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # Streamlit sayfasını tam ekran yap
-st.set_page_config(layout="wide", page_title="İş Takip Sistemi")
+st.set_page_config(layout="full width", page_title="İş Takip Sistemi")
 
 # HTML dosyasını oku
 with open("index.html", "r", encoding="utf-8") as f:
