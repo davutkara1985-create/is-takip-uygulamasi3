7b38ab767c203790be768646a7445509a14fd300
davutkara1985-create <davutkara1985@gmail.com>
2026-05-14 16:41:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,14 +9,4 @@ with open("index.html", "r", encoding="utf-8") as f:
     html_kodu = f.read()
 
 # HTML'i Streamlit bileşeni olarak ekranda göster
-components.html(
-    html_kodu,
-    height=800,
-    scrolling=True
-)
-<style>
-html, body {
-    height: auto;
-    margin: 0;
-}
-</style>
+components.html(html_kodu, height=3000, scrolling=True)
